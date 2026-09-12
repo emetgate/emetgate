06a7cf97f2877f5507ac43feb6f2e0eb6ac2e134
ugur <infougurcalskan@gmail.com>
2026-09-12 16:29:18 +0300

    test: add ingest-cost benchmark (skeleton+read_symbol vs whole-file)

---
+++ bench/master/big.ts
@@ -0,0 +1,165 @@
+export function op1(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 1;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op2(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 2;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op3(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 3;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op4(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 4;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op5(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 5;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op6(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 6;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op7(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 7;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op8(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 8;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op9(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 9;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op10(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 10;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op11(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 11;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op12(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 12;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op13(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 13;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op14(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 14;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
+export function op15(a: number, b: number): number {
+  let r = a;
+  for (let k = 0; k < b; k++) {
+    r = r + 15;
+  }
+  if (r < 0) {
+    r = -r;
+  }
+  return r;
+}
+
